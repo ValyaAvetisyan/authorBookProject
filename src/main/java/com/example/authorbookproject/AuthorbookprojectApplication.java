2451dd4f83e9f5fca1f4e3e21a40f018c173dc62
ValyaAvetisyan <valyaavetisyan@gmail.com>
2018-02-19 22:47:11 +0400

    add user model

---
+++ src/main/java/com/example/authorbookproject/AuthorbookprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.authorbookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorbookprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorbookprojectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/authorbookproject/model/User.java
@@ -0,0 +1,11 @@
+package com.example.authorbookproject.model;
+
+import lombok.Data;
+
+@Data
+public class User
+{
+    private  int id;
+    private String name;
+    private String surname;
+}
